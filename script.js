e1520a62f2c81081b0fbbd120dc2e6e3029c8aea
LunarWorldYT <145892925+LunarWorldYT@users.noreply.github.com>
2024-02-05 08:10:44 -0600

    copy pasted bullshit from gpt once again, looks close enough to the actual code so must work?

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global variables
 
 var count = 0;
 var Money = 0;
@@ -7,80 +8,76 @@ var makeButton = document.getElementById('Make');
 var countDisplay = document.getElementById('count');
 var moneyDisplay = document.getElementById('Money');
 
-// Event listener for the 'Make' button
-makeButton.addEventListener('click', function() {
- count += baseMake;
- updateDisplay();
- console.log("make was pressed");
- console.log("count is now: " + count);
- countDisplay.textContent = "Count: " + count;
- renderUpgrades();
-})
-
-// Event listener for the 'Sell' button
-sellButton.addEventListener('click', function() {
-if (count > 0){ 
-count--
-Money++
- }
- countDisplay.textContent = "Count: " + count;
- moneyDisplay.textContent = "Money: " + Money;
- renderUpgrades();
-})
-
-// Event listener for the 'brainrot' button
-window.onload = function() {
-var brainrotButton = document.getElementById('brainrot'); // Changed the variable name to 'brainrotButton'
-var brainrotvideo = document.getElementById('brainrotvideo');
-
-brainrotButton.addEventListener('click', function(){ // Changed 'brainrotmode' to 'brainrotButton'
-brainrotvideo.style.display = 'block';
- })
-}
-
 class Upgrade {
- constructor(name, cost, effect) {
+  constructor(name, cost, effect) {
     this.name = name;
     this.cost = cost;
     this.effect = effect || (() => {});
     this.count = 0;
+  }
+  apply() {
+    this.effect();
+  }
 
- }
- apply() {
-  this.effect();
-
- }
- 
-
- render(upgradesDiv) {
-     var button = document.createElement('button');
-button.innerText = this.name + " (" + this.cost + " Money)";
-button.disabled = Money < this.cost;
-button.onclick = () => {
-if(Money >= this.cost){
- Money -= this.cost;
- this.count++;
- this.apply();
- updateDisplay();
- renderUpgrades(); 
- }
-}
-  upgradesDiv.appendChild(button);
+  render(upgradesDiv) {
+    var button = document.createElement('button');
+    button.innerText = this.name + " (" + this.cost + " Money)";
+    button.disabled = Money < this.cost;
+    button.onclick = () => {
+      if(Money >= this.cost){
+        Money -= this.cost;
+        this.count++;
+        this.apply();
+        updateDisplay();
+        renderUpgrades();
+      }
+    }
+    upgradesDiv.appendChild(button);
+  }
 }
 
+// Define upgrade instances
+var upgrade1 = new Upgrade("Upgrade 1", 100, function(){ baseMake += 2; })
+var upgrade2 = new Upgrade("Upgrade 2", 250, function(){ Money += 2; })
+
+function updateDisplay(){
+  countDisplay.textContent = "Count: " + count;
+  moneyDisplay.textContent = "Money: " + Money;
+  renderUpgrades();
 }
 
 function renderUpgrades(){
-var upgradesDiv = document.getElementById('upgrades');
-upgradesDiv.innerHTML = '';
+  var upgradesDiv = document.getElementById('upgrades');
+  upgradesDiv.innerHTML = '';
 
-for(let upgrade of [upgrade1, upgrade2]){
-upgrade.render(upgradesDiv);
+  for(let upgrade of [upgrade1, upgrade2]){
+    upgrade.render(upgradesDiv);
+  }
 }
 
+// Event listener for the 'Make' button
+makeButton.addEventListener('click', function() {
+  count += baseMake;
+  console.log("make was pressed");
+  console.log("count is now: " + count);
+  updateDisplay();
+})
 
-}
+sellButton.addEventListener('click', function() {
+  if (count > 0){
+    count--;
+    Money++;
+    updateDisplay();
+  }
+})
+
+// This ensures all event listeners get properly added after page load.
 window.onload = function() {
-var upgrade1 = new Upgrade("Upgrade 1", 100, function(){ baseMake += 2; })
-var upgrade2 = new Upgrade("Upgrade 2", 250, function(){ Money += 2; })
-renderUpgrades();}
+  var brainrotButton = document.getElementById('brainrot'); // Changed the variable name to 'brainrotButton'
+  var brainrotvideo = document.getElementById('brainrotvideo');
+  brainrotButton.addEventListener('click', function(){ 
+   brainrotvideo.style.display = 'block';
+  });
+
+  renderUpgrades();
+}
